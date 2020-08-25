049a277f8c6ab9232a66734942b8344cce8876b1
James Marshall <68325922+JamesMarshall31@users.noreply.github.com>
2020-08-25 18:18:24 +0100

    Update doe_class.py

--- src/doe_class.py
+++ src/doe_class.py
@@ -6,7 +6,7 @@ import itertools
 class Factorial():
     def full_factorial(num_factors):
         df = pd.DataFrame()
-        for run in itertools.product([-1, 1],repeat=num_factors)
+        for run in itertools.product([-1, 1],repeat=num_factors):
             s_add = pd.Series(run)
             df = pd.concat([df,s_add],axis=1,ignore_index=True)
         return df.transpose()
