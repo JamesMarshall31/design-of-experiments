a6f65c27277954e6c44334deaa467a9823646c29
James Marshall <68325922+JamesMarshall31@users.noreply.github.com>
2020-08-25 17:19:10 +0100

    Update doe_class.py

--- src/doe_class.py
+++ src/doe_class.py
@@ -1,1 +1,16 @@
 # Our Design of experiment class
+
+import pandas as pd
+import itertools
+
+class Factorial():
+    def full_factorial(num_factors):
+        df = pd.DataFrame()
+        factor_data = []
+        for run in itertools.product([-1, 1],repeat=num_factors):
+            print(run)
+            s_add = pd.Series(run)
+            df = pd.concat([df,s_add],axis=1)
+        return df.transpose()
+
+
