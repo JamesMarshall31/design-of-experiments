5e4e65449ed029764ad0ed13bfca92302314a4d9
Benedict Carling <benedict.carling18@imperial.ac.uk>
2020-08-25 16:18:13 +0100

    adds MIT license and setup.py

---
+++ setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name='designofexperiment',
+    version='1.0.0',
+    description='A Python Package for intuitive design of experiments with user-friendly analysis of results',
+    py_modules=["doe_class"],
+    package_dir={'': 'src'},
+    classifiers = [
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Operating System :: OS Independent"
+    ],
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    extras_require={
+        "dev": [
+            "pytest>=6.0.1",
+            "check-manifest>=0.42",
+            "twine>=3.2.0"
+        ],
+    },
+    url="https://github.com/JamesMarshall31/design-of-experiments",
+    author="James Marshall, Benedict Carling",
+    author_email="jm7618@ic.ac.uk, bencarling1@gmail.com"
+)
