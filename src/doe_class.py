18c906a1b05c093224df877173c8c399b216e8e1
James <JM7618@ic.ac.uk>
2020-08-28 23:22:47 +0100

    Fixed Docstring

--- src/doe_class.py
+++ src/doe_class.py
@@ -1,20 +1,21 @@
 # Our Design of experiment class
 
-"""
-Creates a Two-level full factorial design, from the dictionary of Factors entered
 
-example:
-
-Factors = {'Height':[1.6,2],'Width':[0.2,0.4],'Depth':[0.2,0.3]}
-doe_class.Factorial.full_factorial(Factors)
-
-"""
 import numpy as np
 import pandas as pd
 import itertools
 
 class Factorial():
     def full_factorial(dic_factors):
+        """
+        Creates a Two-level full factorial design, from the dictionary of Factors entered
+
+        example:
+
+        Factors = {'Height':[1.6,2],'Width':[0.2,0.4],'Depth':[0.2,0.3]}
+        doe_class.Factorial.full_factorial(Factors)
+
+        """
         df = pd.DataFrame()
         factor_levels =[]
         factor_names=[]
