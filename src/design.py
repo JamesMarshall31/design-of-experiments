75aacd76cc47488c5b2c38769d4e1e045b1a2d4d
James <JM7618@ic.ac.uk>
2020-08-31 20:06:55 +0100

    Have commented code

--- src/design.py
+++ src/design.py
@@ -1,10 +1,9 @@
 # Our Design of experiment class
 
-
-import numpy as np
 import pandas as pd
 import itertools
 import math
+
 
 class Factorial():
     def full_factorial_2level(dic_factors):
@@ -32,18 +31,32 @@ class Factorial():
             6     2.0    0.4    0.2
             7     2.0    0.4    0.3
         """
+        # df is the dataframe that will be returned.
         df = pd.DataFrame()
-        factor_levels =[]
-        factor_names=[]
+        # factor_levels will be filled with the levels of each factor and
+        # used when iterating through the runs of the design.
+        factor_levels = []
+        # factor_names is filled at the same time as factor_levels and
+        # is used at the end to correctly name the columns of the dataframe.
+        factor_names = []
 
+        # This for loop fills up factor_levels with the maximum and minimum of each factor,
+        # as well as filling up factor_names.
         for name in dic_factors:
             factor_names.append(name)
-            factor_levels.append([min(dic_factors[name]),max(dic_factors[name])])
+            factor_levels.append([min(dic_factors[name]), max(dic_factors[name])])
+
+        # This for loop will run through each combination(technically product) and build up
+        # the dataframe df with each loop.
         for run in itertools.product(*factor_levels, repeat=1):
             run = list(run)
             s_add = pd.Series(run)
             df = pd.concat([df, s_add], axis=1, ignore_index=True)
+        # The dataframe is made with the runs being the columns, we want them to be the rows
+        # hence the need to transpose.
         df = df.transpose()
+        # The column headers are initially labelled '0','1','2' etc.., the line below
+        # renames them by relating them to the factor_names list made earlier
         df = df.rename(columns=lambda x: factor_names[x])
         return df
 
@@ -73,21 +86,30 @@ class Factorial():
             7     2.0    0.3
             8     2.0    0.4
         """
+        # The variables initialised below play the same role here as in the two level
+        # full factorial above.
         df = pd.DataFrame()
         factor_levels = []
         factor_names = []
+
+        # This for loop plays the same role as the for loop in the two level
+        # but does not take the maximum and minimum factor levels, so does not reduce
+        # the design to a two level design automatically.
         for name in dic_factors:
             factor_names.append(name)
             factor_levels.append(dic_factors[name])
+
+        # This for loop functions the same as its two level counterpart.
         for run in itertools.product(*factor_levels, repeat=1):
             run = list(run)
             s_add = pd.Series(run)
             df = pd.concat([df, s_add], axis=1, ignore_index=True)
+        # As in the two level, the dataframe must be transposed and renamed.
         df = df.transpose()
         df = df.rename(columns=lambda x: factor_names[x])
         return df
 
-    def frac_fact_2level(dic_factors,runs):
+    def frac_fact_2level(dic_factors, runs):
         """
         Returns a fractional factorial based on the dictionary of factors entered and the runs entered,
          the number of runs of the design will be the next lowest power of 2 from the runs entered
@@ -119,41 +141,77 @@ class Factorial():
         df = pd.DataFrame()
         factor_levels = []
         factor_names = []
+
         for name in dic_factors:
             factor_names.append(name)
-            factor_levels.append([min(dic_factors[name]),max(dic_factors[name])])
-        runs = int((1<<(runs).bit_length())/2) # if runs entered isn't a power of 2 this this will set it to the
-        # next lowest power of 2
-        full_fact_level = int(math.log(runs,2))
+            factor_levels.append([min(dic_factors[name]), max(dic_factors[name])])
+
+        # If runs entered isn't a power of 2 this will set it to the
+        # next lowest power of 2.
+        runs = int((1 << (runs).bit_length()) / 2)
+        # The fractional factorial is generated from a base full factorial
+        # see https://www.itl.nist.gov/div898/handbook/pri/section3/pri3342.htm
+        # the line below determines the level of this full fact level
+        full_fact_level = int(math.log(runs, 2))
+
+        # This for loop creates the base Full Factorial from which the
+        # fractional factorial will be generated
         for run in itertools.product([-1, 1], repeat=full_fact_level):
             run = list(run)
             s_add = pd.Series(run)
             df = pd.concat([df, s_add], axis=1, ignore_index=True)
         df = df.transpose()
-        factors_remaining = len(factor_names)-full_fact_level
+        # factors_remaining will be used in the coming loops - it measures how many columns
+        # are left to be added to the fractional factorial.
+        factors_remaining = len(factor_names) - full_fact_level
+        # count will be used for the creation of new columns in the dataframe.
         count = 0
+        # df_cols is quite literally a list of the column headers in the dataframe
+        # the columns are automatically name as if in an array 0,1,2 etc..
         df_cols = []
+        # r is used in the itertools combination function, it is set to two as
+        # the first columns of the fractional factorial are the two combinations,
+        # it is incremented as higher combinations are required.
         r = 2
+        # r will need to be incremented when all combinations at its current level have been
+        # added, combination check is used in a conditional that will increase r and
+        # reinitialise the for loop that r is used in.
         combination_check = 0
+
+        # This for loop fills up df_cols with the columns already made in the base full factorial
         for x in range((len(dic_factors) - factors_remaining)):
             df_cols.append(x)
-        while factors_remaining>0:
+
+        # The while loop is here to reinitialise the for loop once r has changed, and
+        # to stop the for loop creating too many columns once factors_remaining is 0.
+        while factors_remaining > 0:
+            # As each new column is the combination of columns from the base full factorial
+            # this for loop goes through each combination and creates these columns.
             for combination in itertools.combinations(df_cols, r):
+                # We initialise the values as 1 so that we can multiply them by however much
+                # is necessary in the coming for loop.
                 df[full_fact_level + count] = 1
+
+                # The for loop here goes up to r because if r is two the result will be the multiplication
+                # of two columns, if r is three, three columns etc..
                 for i in range(r):
-                    df[full_fact_level+count] = df[full_fact_level+count]*df[combination[i]]
+                    df[full_fact_level + count] = df[full_fact_level + count] * df[combination[i]]
                 count += 1
                 factors_remaining -= 1
-                combination_check+=1
+                combination_check += 1
+
+                # If we have run through all combinations with this 'r' value
+                # we should increase r.
                 if combination_check == len(list(itertools.combinations(df_cols, r))):
-                    r+=1
-                    combination_check=0
+                    r += 1
+                    combination_check = 0
                     break
-                if factors_remaining==0:
+                # If there are no factors left then we should stop adding columns.
+                if factors_remaining == 0:
                     break
-        # here we need to add columns to the dataframe that are the products of the other columns
-        # but the columns chosen to be used will change depending on the factors that are left
+        # The dataframe is currently -1 and 1, this for loop assigns the right levels to
+        # each factor using the factor_levels list
         for i in range(len(dic_factors)):
-            df[i] = df[i].apply(lambda x: factor_levels[i][0] if x==-1 else factor_levels[i][1])
-        df = df.rename(columns=lambda x: factor_names[x])
+            df[i] = df[i].apply(lambda y: factor_levels[i][0] if y == -1 else factor_levels[i][1])
+        df = df.rename(columns=lambda y: factor_names[y])
         return df
